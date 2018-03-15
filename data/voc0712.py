b751084b8c233fe01da5e586cfcf6f49da491ef7
ml9951 <ml9951@rit.edu>
2018-03-15 13:43:43 -0400

    Clean up

--- data/voc0712.py
+++ data/voc0712.py
@@ -6,8 +6,9 @@ https://github.com/fmassa/vision/blob/voc_dataset/torchvision/datasets/voc.py
 Updated by: Ellis Brown, Max deGroot
 """
 
+import pdb
 import os
-import os.path
+import glob
 import sys
 import torch
 import torch.utils.data as data
@@ -31,160 +32,48 @@ VOC_CLASSES = (  # always index 0
 COLORS = ((255, 0, 0, 128), (0, 255, 0, 128), (0, 0, 255, 128),
           (0, 255, 255, 128), (255, 0, 255, 128), (255, 255, 0, 128))
 
-
-class AnnotationTransform(object):
-    """Transforms a VOC annotation into a Tensor of bbox coords and label index
-    Initilized with a dictionary lookup of classnames to indexes
-
-    Arguments:
-        class_to_ind (dict, optional): dictionary lookup of classnames -> indexes
-            (default: alphabetic indexing of VOC's 20 classes)
-        keep_difficult (bool, optional): keep difficult instances or not
-            (default: False)
-        height (int): height
-        width (int): width
-    """
-
-    def __init__(self, class_to_ind=None, keep_difficult=False):
-        self.class_to_ind = class_to_ind or dict(
-            zip(VOC_CLASSES, range(len(VOC_CLASSES))))
-        self.keep_difficult = keep_difficult
-
-    def __call__(self, target, width, height):
-        """
-        Arguments:
-            target (annotation) : the target annotation to be made usable
-                will be an ET.Element
-        Returns:
-            a list containing lists of bounding boxes  [bbox coords, class name]
-        """
-        res = []
-        for obj in target.iter('object'):
-            difficult = int(obj.find('difficult').text) == 1
-            if not self.keep_difficult and difficult:
-                continue
-            name = obj.find('name').text.lower().strip()
-            bbox = obj.find('bndbox')
-
-            pts = ['xmin', 'ymin', 'xmax', 'ymax']
-            bndbox = []
-            for i, pt in enumerate(pts):
-                cur_pt = int(bbox.find(pt).text) - 1
-                # scale height or width
-                cur_pt = cur_pt / width if i % 2 == 0 else cur_pt / height
-                bndbox.append(cur_pt)
-            label_idx = self.class_to_ind[name]
-            bndbox.append(label_idx)
-            res += [bndbox]  # [xmin, ymin, xmax, ymax, label_ind]
-            # img_id = target.find('filename').text[:-4]
-
-        return res  # [[xmin, ymin, xmax, ymax, label_ind], ... ]
-
-
-class VOCDetection(data.Dataset):
-    """VOC Detection Dataset Object
-
-    input is image, target is annotation
-
-    Arguments:
-        root (string): filepath to VOCdevkit folder.
-        image_set (string): imageset to use (eg. 'train', 'val', 'test')
-        transform (callable, optional): transformation to perform on the
-            input image
-        target_transform (callable, optional): transformation to perform on the
-            target `annotation`
-            (eg: take in caption string, return tensor of word indices)
-        dataset_name (string, optional): which dataset to load
-            (default: 'VOC2007')
-    """
-
-    def __init__(self, root, image_sets, transform=None, target_transform=None,
-                 dataset_name='VOC0712'):
-        self.root = root
-        self.image_set = image_sets
+class VOC(data.Dataset):
+    name = 'VOC'
+    def __init__(self, root_dir = '../data/VOCdevkit/VOC2007', keep_difficult=False, transform = None):
         self.transform = transform
-        self.target_transform = target_transform
-        self.name = dataset_name
-        self._annopath = os.path.join('%s', 'Annotations', '%s.xml')
-        self._imgpath = os.path.join('%s', 'JPEGImages', '%s.jpg')
-        self.ids = list()
-        for (year, name) in image_sets:
-            rootpath = os.path.join(self.root, 'VOC' + year)
-            for line in open(os.path.join(rootpath, 'ImageSets', 'Main', name + '.txt')):
-                self.ids.append((rootpath, line.strip()))
+        self.root_dir = root_dir
+        anno_files = glob.glob(os.path.join(root_dir, 'Annotations/*.xml'))
+        class_map = {k : i for i, k in enumerate(VOC_CLASSES)}
 
-    def __getitem__(self, index):
-        im, gt, h, w = self.pull_item(index)
+        self.annos = []
+        for file in anno_files:
+            anno = ET.fromstring(open(file).read())
+            imgfile = os.path.join(root_dir, 'JPEGImages', anno.find('filename').text)
+            height = float(anno.find('size').find('height').text)
+            width = float(anno.find('size').find('width').text)
 
-        return im, gt
+            current = {'img' : imgfile, 'objects' : [], 'height': height, 'width': width}
+
+            for obj in anno.findall('object'):
+                bbox = obj.find('bndbox')
+                if keep_difficult or obj.find('difficult').text == '0':
+                    current['objects'].append((
+                        float(bbox.find('xmin').text) / width,
+                        float(bbox.find('ymin').text) / height,
+                        float(bbox.find('xmax').text) / width,
+                        float(bbox.find('ymax').text) / height,
+                        float(class_map[obj.find('name').text])
+                    ))
+            if len(current['objects']) > 0:
+                self.annos.append(current)
 
     def __len__(self):
-        return len(self.ids)
+        return len(self.annos)
 
-    def pull_item(self, index):
-        img_id = self.ids[index]
-
-        target = ET.parse(self._annopath % img_id).getroot()
-        img = cv2.imread(self._imgpath % img_id)
-        height, width, channels = img.shape
-
-        if self.target_transform is not None:
-            target = self.target_transform(target, width, height)
-
-        if self.transform is not None:
-            target = np.array(target)
+    def __getitem__(self, idx):
+        anno = self.annos[idx]
+        img = cv2.imread(anno['img'])
+        target = np.array(anno['objects'])
+        if self.transform:
             img, boxes, labels = self.transform(img, target[:, :4], target[:, 4])
-            # to rgb
-            img = img[:, :, (2, 1, 0)]
-            # img = img.transpose(2, 0, 1)
+            img = img.transpose((2, 0, 1))[(2, 1, 0), :, :]
             target = np.hstack((boxes, np.expand_dims(labels, axis=1)))
-        return torch.from_numpy(img).permute(2, 0, 1), target, height, width
-        # return torch.from_numpy(img), target, height, width
-
-    def pull_image(self, index):
-        '''Returns the original image object at index in PIL form
-
-        Note: not using self.__getitem__(), as any transformations passed in
-        could mess up this functionality.
-
-        Argument:
-            index (int): index of img to show
-        Return:
-            PIL img
-        '''
-        img_id = self.ids[index]
-        return cv2.imread(self._imgpath % img_id, cv2.IMREAD_COLOR)
-
-    def pull_anno(self, index):
-        '''Returns the original annotation of image at index
-
-        Note: not using self.__getitem__(), as any transformations passed in
-        could mess up this functionality.
-
-        Argument:
-            index (int): index of img to get annotation of
-        Return:
-            list:  [img_id, [(label, bbox coords),...]]
-                eg: ('001718', [('dog', (96, 13, 438, 332))])
-        '''
-        img_id = self.ids[index]
-        anno = ET.parse(self._annopath % img_id).getroot()
-        gt = self.target_transform(anno, 1, 1)
-        return img_id[1], gt
-
-    def pull_tensor(self, index):
-        '''Returns the original image at an index in tensor form
-
-        Note: not using self.__getitem__(), as any transformations passed in
-        could mess up this functionality.
-
-        Argument:
-            index (int): index of img to show
-        Return:
-            tensorized version of img, squeezed
-        '''
-        return torch.Tensor(self.pull_image(index)).unsqueeze_(0)
-
+        return torch.from_numpy(img).float(), torch.from_numpy(target).float(), anno['height'], anno['width']
 
 def detection_collate(batch):
     """Custom collate fn for dealing with batches of images that have a different
@@ -198,9 +87,6 @@ def detection_collate(batch):
             1) (tensor) batch of images stacked on their 0 dim
             2) (list of tensors) annotations for a given image are stacked on 0 dim
     """
-    targets = []
-    imgs = []
-    for sample in batch:
-        imgs.append(sample[0])
-        targets.append(torch.FloatTensor(sample[1]))
+    imgs, targets, heights, widths = zip(*batch)
     return torch.stack(imgs, 0), targets
+    
--- train.py
+++ train.py
@@ -1,221 +1,92 @@
+#!/usr/bin/env python
 import os
 import torch
-import torch.nn as nn
 import torch.optim as optim
-import torch.backends.cudnn as cudnn
-import torch.nn.init as init
-import argparse
-from torch.autograd import Variable
 import torch.utils.data as data
-from data import v2, v1, AnnotationTransform, VOCDetection, detection_collate, VOCroot, VOC_CLASSES
+from torch.autograd import Variable
+import argparse
+from data import detection_collate, VOCroot, VOC_CLASSES, VOC
 from utils.augmentations import SSDAugmentation
 from layers.modules import MultiBoxLoss
 from ssd import build_ssd
 import numpy as np
 import time
+from subprocess import check_output
+from datetime import datetime
 
 def str2bool(v):
     return v.lower() in ("yes", "true", "t", "1")
 
-parser = argparse.ArgumentParser(description='Single Shot MultiBox Detector Training')
-parser.add_argument('--version', default='v2', help='conv11_2(v2) or pool6(v1) as last layer')
-parser.add_argument('--basenet', default='vgg16_reducedfc.pth', help='pretrained base model')
-parser.add_argument('--jaccard_threshold', default=0.5, type=float, help='Min Jaccard index for matching')
-parser.add_argument('--batch_size', default=32, type=int, help='Batch size for training')
-parser.add_argument('--resume', default=None, type=str, help='Resume from checkpoint')
-parser.add_argument('--num_workers', default=4, type=int, help='Number of workers used in dataloading')
-parser.add_argument('--iterations', default=120000, type=int, help='Number of training iterations')
-parser.add_argument('--start_iter', default=0, type=int, help='Begin counting iterations starting from this value (should be used with resume)')
-parser.add_argument('--cuda', default=True, type=str2bool, help='Use cuda to train model')
-parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, help='initial learning rate')
-parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
-parser.add_argument('--weight_decay', default=5e-4, type=float, help='Weight decay for SGD')
-parser.add_argument('--gamma', default=0.1, type=float, help='Gamma update for SGD')
-parser.add_argument('--log_iters', default=True, type=bool, help='Print the loss at each iteration')
-parser.add_argument('--visdom', default=False, type=str2bool, help='Use visdom to for loss visualization')
-parser.add_argument('--send_images_to_visdom', type=str2bool, default=False, help='Sample a random image from each 10th batch, send it to visdom after augmentations step')
-parser.add_argument('--save_folder', default='weights/', help='Location to save checkpoint models')
-parser.add_argument('--voc_root', default=VOCroot, help='Location of VOC root directory')
-args = parser.parse_args()
-
-if args.cuda and torch.cuda.is_available():
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
-else:
-    torch.set_default_tensor_type('torch.FloatTensor')
-
-cfg = (v1, v2)[args.version == 'v2']
-
-if not os.path.exists(args.save_folder):
-    os.mkdir(args.save_folder)
-
-train_sets = [('2007', 'trainval'), ('2012', 'trainval')]
-# train_sets = 'train'
-ssd_dim = 300  # only support 300 now
-means = (104, 117, 123)  # only support voc now
-num_classes = len(VOC_CLASSES) + 1
-batch_size = args.batch_size
-accum_batch_size = 32
-iter_size = accum_batch_size / batch_size
-max_iter = 120000
-weight_decay = 0.0005
-stepvalues = (80000, 100000, 120000)
-gamma = 0.1
-momentum = 0.9
-
-if args.visdom:
-    import visdom
-    viz = visdom.Visdom()
-
-ssd_net = build_ssd('train', 300, num_classes)
-net = ssd_net
-
-if args.cuda:
-    net = torch.nn.DataParallel(ssd_net)
-    cudnn.benchmark = True
-
-if args.resume:
-    print('Resuming training, loading {}...'.format(args.resume))
-    ssd_net.load_weights(args.resume)
-else:
-    vgg_weights = torch.load(args.save_folder + args.basenet)
-    print('Loading base network...')
-    ssd_net.vgg.load_state_dict(vgg_weights)
-
-if args.cuda:
-    net = net.cuda()
-
-
-def xavier(param):
-    init.xavier_uniform(param)
-
-
-def weights_init(m):
-    if isinstance(m, nn.Conv2d):
-        xavier(m.weight.data)
+def init_weights(m):
+    if isinstance(m, torch.nn.Conv2d):
+        torch.nn.init.xavier_uniform(m.weight.data)
         m.bias.data.zero_()
 
+def save_checkpoint(net, args, iter, filename):
+    os.makedirs(args.checkpoint_dir, exist_ok=True)
+    torch.save({
+        'iter': iter,
+        'state_dict' : net.module.state_dict()
+    }, filename)
 
-if not args.resume:
-    print('Initializing weights...')
-    # initialize newly added layers' weights with xavier method
-    ssd_net.extras.apply(weights_init)
-    ssd_net.loc.apply(weights_init)
-    ssd_net.conf.apply(weights_init)
+def load_checkpoint(net, args):
+    if args.resume:
+        chkpnt = torch.load(args.resume)
+        args.start_iter = chkpnt['iter']
+        net.load_state_dict(chkpnt['state_dict'])
+    else:
+        if not os.path.exists(os.path.join(args.save_folder, args.basenet)):
+            url = 'https://s3.amazonaws.com/amdegroot-models/vgg16_reducedfc.pth'
+            check_output(['wget', url, '-O', os.path.join(args.save_folder, args.basenet)])
 
-optimizer = optim.SGD(net.parameters(), lr=args.lr,
+        vgg_weights = torch.load(os.path.join(args.save_folder, args.basenet))
+        net.vgg.load_state_dict(vgg_weights)
+        net.extras.apply(init_weights)
+        net.loc.apply(init_weights)
+        net.conf.apply(init_weights)
+
+def train(net, args):
+    optimizer = optim.SGD(net.parameters(), lr=args.lr,
                       momentum=args.momentum, weight_decay=args.weight_decay)
-criterion = MultiBoxLoss(num_classes, 0.5, True, 0, True, 3, 0.5, False, args.cuda)
+    criterion = MultiBoxLoss(args.num_classes, 0.5, True, 0, True, 3, 0.5, False, args.cuda)
 
-
-def train():
     net.train()
     # loss counters
     loc_loss = 0  # epoch
     conf_loss = 0
-    epoch = 0
-    print('Loading Dataset...')
 
-    dataset = VOCDetection(args.voc_root, train_sets, SSDAugmentation(
-        ssd_dim, means), AnnotationTransform())
-
-    epoch_size = len(dataset) // args.batch_size
-    print('Training SSD on', dataset.name)
-    step_index = 0
-    if args.visdom:
-        # initialize visdom loss plot
-        lot = viz.line(
-            X=torch.zeros((1,)).cpu(),
-            Y=torch.zeros((1, 3)).cpu(),
-            opts=dict(
-                xlabel='Iteration',
-                ylabel='Loss',
-                title='Current SSD Training Loss',
-                legend=['Loc Loss', 'Conf Loss', 'Loss']
-            )
-        )
-        epoch_lot = viz.line(
-            X=torch.zeros((1,)).cpu(),
-            Y=torch.zeros((1, 3)).cpu(),
-            opts=dict(
-                xlabel='Epoch',
-                ylabel='Loss',
-                title='Epoch SSD Training Loss',
-                legend=['Loc Loss', 'Conf Loss', 'Loss']
-            )
-        )
-    batch_iterator = None
-    data_loader = data.DataLoader(dataset, batch_size, num_workers=args.num_workers,
+    dataset = VOC(args.voc_root, transform=SSDAugmentation(args.ssd_size, args.means))
+    data_loader = data.DataLoader(dataset, args.batch_size, num_workers=args.num_workers,
                                   shuffle=True, collate_fn=detection_collate, pin_memory=True)
-    for iteration in range(args.start_iter, max_iter):
-        if (not batch_iterator) or (iteration % epoch_size == 0):
-            # create batch iterator
-            batch_iterator = iter(data_loader)
-        if iteration in stepvalues:
-            step_index += 1
-            adjust_learning_rate(optimizer, args.gamma, step_index)
-            if args.visdom:
-                viz.line(
-                    X=torch.ones((1, 3)).cpu() * epoch,
-                    Y=torch.Tensor([loc_loss, conf_loss,
-                        loc_loss + conf_loss]).unsqueeze(0).cpu() / epoch_size,
-                    win=epoch_lot,
-                    update='append'
-                )
-            # reset epoch loss counters
-            loc_loss = 0
-            conf_loss = 0
-            epoch += 1
+    N = len(data_loader)
 
-        # load train data
-        images, targets = next(batch_iterator)
+    mk_var = lambda x: Variable(x.cuda() if args.cuda else x)
 
-        if args.cuda:
-            images = Variable(images.cuda())
-            targets = [Variable(anno.cuda(), volatile=True) for anno in targets]
-        else:
-            images = Variable(images)
-            targets = [Variable(anno, volatile=True) for anno in targets]
-        # forward
-        t0 = time.time()
-        out = net(images)
-        # backprop
-        optimizer.zero_grad()
-        loss_l, loss_c = criterion(out, targets)
-        loss = loss_l + loss_c
-        loss.backward()
-        optimizer.step()
-        t1 = time.time()
-        loc_loss += loss_l.data[0]
-        conf_loss += loss_c.data[0]
-        if iteration % 10 == 0:
-            print('Timer: %.4f sec.' % (t1 - t0))
-            print('iter ' + repr(iteration) + ' || Loss: %.4f ||' % (loss.data[0]), end=' ')
-            if args.visdom and args.send_images_to_visdom:
-                random_batch_index = np.random.randint(images.size(0))
-                viz.image(images.data[random_batch_index].cpu().numpy())
-        if args.visdom:
-            viz.line(
-                X=torch.ones((1, 3)).cpu() * iteration,
-                Y=torch.Tensor([loss_l.data[0], loss_c.data[0],
-                    loss_l.data[0] + loss_c.data[0]]).unsqueeze(0).cpu(),
-                win=lot,
-                update='append'
-            )
-            # hacky fencepost solution for 0th epoch plot
-            if iteration == 0:
-                viz.line(
-                    X=torch.zeros((1, 3)).cpu(),
-                    Y=torch.Tensor([loc_loss, conf_loss,
-                        loc_loss + conf_loss]).unsqueeze(0).cpu(),
-                    win=epoch_lot,
-                    update=True
-                )
-        if iteration % 5000 == 0:
-            print('Saving state, iter:', iteration)
-            torch.save(ssd_net.state_dict(), 'weights/ssd300_0712_' +
-                       repr(iteration) + '.pth')
-    torch.save(ssd_net.state_dict(), args.save_folder + '' + args.version + '.pth')
+    for epoch in range(args.start_iter, args.epochs):
+        for i, (images, targets) in enumerate(data_loader):
+            if epoch in args.stepvalues:
+                adjust_learning_rate(optimizer, args.gamma, epoch)
 
+            images = mk_var(images)
+            targets = [mk_var(anno) for anno in targets]
+
+            # forward
+            out = net(images)
+
+            # backprop
+            optimizer.zero_grad()
+            loss_l, loss_c = criterion(out, targets)
+            loss = loss_l + loss_c
+            loss.backward()
+            optimizer.step()
+
+
+            loc_loss += loss_l.data[0]
+            conf_loss += loss_c.data[0]
+
+            print('%d: [%d/%d] || Loss: %.4f' % (epoch, i, N, loss.data[0]))
+
+        save_checkpoint(net, args, epoch, os.path.join(args.checkpoint_dir, 'epoch_%d.pth' % epoch))
 
 def adjust_learning_rate(optimizer, gamma, step):
     """Sets the learning rate to the initial LR decayed by 10 at every specified step
@@ -228,4 +99,53 @@ def adjust_learning_rate(optimizer, gamma, step):
 
 
 if __name__ == '__main__':
-    train()
+    parser = argparse.ArgumentParser(description='Single Shot MultiBox Detector Training')
+    parser.add_argument('--version', default='v2', help='conv11_2(v2) or pool6(v1) as last layer')
+    parser.add_argument('--basenet', default='vgg16_reducedfc.pth', help='pretrained base model')
+    parser.add_argument('--jaccard_threshold', default=0.5, type=float, help='Min Jaccard index for matching')
+    parser.add_argument('--batch_size', default=32, type=int, help='Batch size for training')
+    parser.add_argument('--resume', default=None, type=str, help='Resume from checkpoint')
+    parser.add_argument('--num_workers', default=4, type=int, help='Number of workers used in dataloading')
+    parser.add_argument('--iterations', default=120000, type=int, help='Number of training iterations')
+    parser.add_argument('--cuda', default=True, type=str2bool, help='Use cuda to train model')
+    parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, help='initial learning rate')
+    parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+    parser.add_argument('--weight_decay', default=5e-4, type=float, help='Weight decay for SGD')
+    parser.add_argument('--gamma', default=0.1, type=float, help='Gamma update for SGD')
+    parser.add_argument('--save_folder', default='weights/', help='Location to save checkpoint models')
+    parser.add_argument('--epochs', default=100, type=int, help='Maximum training epochs')
+    parser.add_argument('--voc_root', default=VOCroot, help='Location of VOC root directory')
+    parser.add_argument('--ssd_size', default=300, type=int, help='Input dimensions for SSD')
+    args = parser.parse_args()
+
+    args.checkpoint_dir = os.path.join(args.save_folder, 'ssd_%s' % datetime.now().isoformat())
+    args.means = (104, 117, 123)  # only support voc now
+    args.num_classes = len(VOC_CLASSES) + 1
+    args.stepvalues = (20, 50, 70)
+    args.start_iter = 0
+
+    os.makedirs(args.save_folder, exist_ok = True)
+
+    default_type = 'torch.cuda.FloatTensor' if args.cuda else 'torch.FloatTensor'
+    torch.set_default_tensor_type(default_type)
+
+    net = build_ssd('train', args.ssd_size, args.num_classes)
+
+    if args.cuda:
+        net = net.cuda()
+
+    load_checkpoint(net, args)
+    train(net, args)
+
+
+
+
+
+
+
+
+
+
+
+
+
